7ec3c8ae3c71990e95557a9b07b2338752399b81
odon-airoldi <odon.airoldi@gmail.com>
2026-03-16 20:30:43 +0100

    add filter using useEffect with a input value dependencty

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,10 @@ function App() {
   const [selectGenre, setSelectGenre] = useState('')
   const [filteredFilms, setFilteredFilms] = useState(films)
 
+  //input
+  const [searchFilm, setSearchFilm] = useState('')
+
+
 
   useEffect(() => {
 
@@ -30,16 +34,37 @@ function App() {
       setFilteredFilms(films)
     )
 
-
   }, [selectGenre]);
 
 
 
-  return (
-    <div className='container py-5'>
+  useEffect(() => {
 
-      <select className='form-select' onChange={(e) => setSelectGenre(e.target.value)}>
-        <option value="">Select by genre</option>
+    if (searchFilm != '') {
+      const resultSearch = films.filter(film => (
+        film.title.toLowerCase().includes(searchFilm.toLowerCase())
+      ))
+      setFilteredFilms(resultSearch)
+    } else {
+      setFilteredFilms(films)
+    }
+
+  }, [searchFilm])
+
+
+  return (
+    <div className="container py-5">
+
+      <div className="mb-5">
+        <div className="row">
+          <div className="col-6">
+            <input className="form-control" type="search" placeholder="Filter by title" onChange={(e) => setSearchFilm(e.target.value)} />
+          </div>
+        </div>
+      </div>
+
+      <select className="form-select" onChange={(e) => setSelectGenre(e.target.value)}>
+        <option value="">Filter by genre</option>
         <option value="Fantascienza">Fantascienza</option>
         <option value="Thriller">Thriller</option>
         <option value="Romantico">Romantico</option>
